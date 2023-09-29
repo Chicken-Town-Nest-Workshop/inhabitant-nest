07f406dbfd861cdcbe8fd561a41ac48214015f66
gson Liang <yuanyu90221@gmail.com>
2023-09-29 13:36:12 +0800

    fix(fix ts-loader probem with update version): fix ts-loader by upgrade
    fix ts-loader by use pnpm up ts-node-dev@latest ts-node@latest

--- src/typeorm.migration.ts
+++ src/typeorm.migration.ts
@@ -5,17 +5,17 @@ import { DataSource } from 'typeorm';
 config();
 const configService = new ConfigService();
 const IS_DB_SSL_MODE = configService.getOrThrow<boolean>(
-    'IS_DB_SSL_MODE',
-    false,
+  'IS_DB_SSL_MODE',
+  false,
 );
 export default new DataSource({
-    type: 'postgres',
-    url: configService.get<string>('DB_URI', ''),
-    ssl: IS_DB_SSL_MODE,
-    extra: {
-        ssl: IS_DB_SSL_MODE ? { rejectUnauthorized: false } : null,
-    },
-    migrations: ['src/migrations/*.ts'],
-    migrationsRun: true,
-    entities: ['**/*.entity.ts'],
+  type: 'postgres',
+  url: configService.get<string>('DB_URI', ''),
+  ssl: IS_DB_SSL_MODE,
+  extra: {
+    ssl: IS_DB_SSL_MODE ? { rejectUnauthorized: false } : null,
+  },
+  migrations: ['src/migrations/*.ts'],
+  migrationsRun: true,
+  entities: ['**/*.entity.ts'],
 });
